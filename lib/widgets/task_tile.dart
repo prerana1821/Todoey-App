2cc1263fdb54ac6bbcdc0340761aab8a3561fbb1
prerana1821 <prerananawar1@gmail.com>
2020-10-08 03:13:37 +0530

    Complete App

---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
+  final Function longPressCallback;
+
+  TaskTile(
+      {this.isChecked,
+      this.taskTitle,
+      this.checkboxCallback,
+      this.longPressCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onLongPress: longPressCallback,
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
+      ),
+    );
+  }
+}
+
+// class TaskCheckbox extends StatelessWidget {
+//   final bool checkBoxState;
+//   final Function toggeleCheckboxState;
+
+//   TaskCheckbox({this.checkBoxState, this.toggeleCheckboxState});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+// }
